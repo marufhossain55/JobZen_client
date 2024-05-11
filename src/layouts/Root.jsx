0ba2a4dd7f413ec943e2148a3de51b680f020678
marufhossain55 <engrmaruf3@gmail.com>
2024-05-11 20:17:05 +0600

    login page added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,18 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import bg from '../assets/bg-main.png';
+// import bg from '../assets/bg-main.png';
 
 const Root = () => {
   return (
     <div
-      className='font-workSans'
-      style={{
-        // background: 'linear-gradient(62deg, #E0F9ED 0%, #DCEEF7 100%)',
-        background: `URL(${bg})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
+      className='font-workSans '
+      // style={{
+      //   // background: 'linear-gradient(62deg, #E0F9ED 0%, #DCEEF7 100%)',
+      //   background: `URL(${bg})`,
+      //   backgroundSize: 'cover',
+      //   backgroundPosition: 'center',
+      // }}
     >
       <div className='container mx-auto'>
         <NavBar></NavBar>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,12 +2,24 @@ import { createBrowserRouter } from 'react-router-dom';
 import Root from '../layouts/Root';
 import ErrorPage from '../layouts/ErrorPage';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [{ path: '/', element: <Home /> }],
+    children: [
+      { path: '/', element: <Home /> },
+      {
+        path: '/login',
+        element: <Login></Login>,
+      },
+      {
+        path: '/register',
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
