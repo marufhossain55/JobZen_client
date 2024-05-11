9b207ee7a0fb4c93e6f52c62a29fbe8b2e64e52e
marufhossain55 <engrmaruf3@gmail.com>
2024-05-11 16:44:29 +0600

    navbar added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from '../layouts/Root';
 import ErrorPage from '../layouts/ErrorPage';
+import Home from '../pages/Home';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [{}],
+    children: [{ path: '/', element: <Home /> }],
   },
 ]);
