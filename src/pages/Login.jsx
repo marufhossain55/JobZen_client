80c2300c68bbb8125522ad34ef566f2059e4bd58
marufhossain55 <engrmaruf3@gmail.com>
2024-05-12 08:01:43 +0600

    login function  page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,41 @@
 import { NavLink } from 'react-router-dom';
 import login from '../assets/login.svg';
+import useAuth from '../hooks/useAuth';
+import { useForm } from 'react-hook-form';
 const Login = () => {
+  const { singInUser, googleSignIn } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleSocialLogin = (socialProvider) => {
+    socialProvider().then((result) => {
+      if (result.user) {
+        alert('Login successfully');
+        // navigate(from);
+      }
+    });
+  };
+
+  const onSubmit = (data) => {
+    const { email, password } = data;
+    console.log(data);
+    singInUser(email, password)
+      .then((result) => {
+        if (result.user) {
+          alert('Login successfully');
+          //   navigate(from);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('please check your email and password');
+      });
+  };
+
   return (
     <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg lg:max-w-4xl'>
       <div className='hidden bg-cover lg:flex lg:w-1/2 border '>
@@ -15,7 +50,7 @@ const Login = () => {
         <p className='mt-3 text-xl text-center text-gray-600 '>Welcome back!</p>
 
         <a
-          href='#'
+          onClick={() => handleSocialLogin(googleSignIn)}
           className='flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50 dark:hover:bg-gray-600'
         >
           <div className='px-4 py-2'>
@@ -58,44 +93,45 @@ const Login = () => {
         </div>
 
         <div className='mt-4'>
-          <label
-            className='block mb-2 text-sm font-medium text-gray-600 '
-            // for='LoggingEmailAddress'
-          >
+          <label className='block mb-2 text-sm font-medium text-gray-600'>
             Email Address
           </label>
           <input
-            id='LoggingEmailAddress'
+            name='email'
             className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
             type='email'
+            {...register('email', { required: true })}
           />
+          {errors.email && (
+            <span className='text-red-500'>This field is required</span>
+          )}
         </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='mt-4'>
+            <div className='flex justify-between'>
+              <label className='block mb-2 text-sm font-medium text-gray-600'>
+                Password
+              </label>
+              <a href='#' className='text-xs text-gray-500  hover:underline'>
+                Forget Password?
+              </a>
+            </div>
 
-        <div className='mt-4'>
-          <div className='flex justify-between'>
-            <label
-              className='block mb-2 text-sm font-medium text-gray-600'
-              //   for='loggingPassword'
-            >
-              Password
-            </label>
-            <a href='#' className='text-xs text-gray-500  hover:underline'>
-              Forget Password?
-            </a>
+            <input
+              className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 d focus:outline-none focus:ring focus:ring-blue-300'
+              type='password'
+              {...register('password', { required: true })}
+            />
           </div>
-
-          <input
-            id='loggingPassword'
-            className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 d focus:outline-none focus:ring focus:ring-blue-300'
-            type='password'
-          />
-        </div>
-
-        <div className='mt-6'>
-          <button className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'>
-            Sign In
-          </button>
-        </div>
+          {errors.password && (
+            <span className='text-red-500'>This field is required</span>
+          )}
+          <div className='mt-6'>
+            <button className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'>
+              Sign In
+            </button>
+          </div>
+        </form>
 
         <div className='flex items-center justify-between mt-4'>
           <span className='w-1/5 border-b md:w-1/4'></span>
