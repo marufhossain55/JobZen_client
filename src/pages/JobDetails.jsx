78b3de1e0658c72d1dd6b15df5f542358a1cc23b
marufhossain55 <engrmaruf3@gmail.com>
2024-05-13 20:57:16 +0600

    single details adding halfaway

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,4 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import JobDetailsCard from '../components/JobDetailsCard';
+
 const JobDetails = () => {
-  return <div>JobDetails</div>;
+  const { id } = useParams();
+  console.log(id);
+  const {
+    isPending,
+    isError,
+    error,
+    data: jobDetails,
+  } = useQuery({
+    queryKey: ['jobDetails'],
+    queryFn: async () => {
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/job/${id}`);
+      return res.data;
+    },
+  });
+  console.log(jobDetails);
+  return (
+    <div>
+      <JobDetailsCard jobDetails={jobDetails} />
+    </div>
+  );
 };
 export default JobDetails;
