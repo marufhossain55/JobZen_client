e0babc40997d58721e83818eeaf972947000a16f
marufhossain55 <engrmaruf3@gmail.com>
2024-05-14 18:25:11 +0600

    form data updated

--- src/pages/AddAJob.jsx
+++ src/pages/AddAJob.jsx
@@ -1,22 +1,57 @@
 import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import useAuth from '../hooks/useAuth';
 const AddAJob = () => {
+  const { user } = useAuth();
   const [startDate, setStartDate] = useState(new Date());
-  console.log(startDate);
+  const [startDateDeadline, setStartDateDeadline] = useState(new Date());
+
+  const handleFromSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userName = user?.displayName;
+    const email = user?.email;
+    const photoURL = form.photoURL.value;
+    const jobCategory = form.jobCategory.value;
+    const jobTitle = form.jobTitle.value;
+    const minSalaryRange = form.minSalaryRange.value;
+    const maxSalaryRange = form.maxSalaryRange.value;
+    const JobDescription = form.JobDescription.value;
+    const jobApplicantsNumber = form.jobApplicantsNumber.value;
+    const jobPostingDate = startDate;
+    const applicationDeadline = startDateDeadline;
+    const jobPostData = {
+      userName,
+      email,
+      photoURL,
+      jobCategory,
+      jobTitle,
+      minSalaryRange,
+      maxSalaryRange,
+      JobDescription,
+      jobApplicantsNumber,
+      jobPostingDate,
+      applicationDeadline,
+    };
+    // console.log(jobPostData);
+  };
+
   return (
     <section className='max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800'>
       <h2 className='text-lg font-semibold text-gray-700 capitalize dark:text-white'>
         Account settings
       </h2>
 
-      <form>
+      <form onSubmit={handleFromSubmit}>
         <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
           <div>
             <label className='text-gray-700 dark:text-gray-200'>
               User Name
             </label>
             <input
+              defaultValue={user?.displayName}
+              disabled
               name='userName'
               type='text'
               className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
@@ -26,6 +61,8 @@ const AddAJob = () => {
           <div>
             <label className='text-gray-700 dark:text-gray-200'>email</label>
             <input
+              defaultValue={user?.email}
+              disabled
               name='email'
               type='email'
               className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
@@ -47,7 +84,7 @@ const AddAJob = () => {
               Job Category
             </label>
             <select
-              name='jobCategory '
+              name='jobCategory'
               className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
             >
               <option value='On-Site'>On-Site</option>
@@ -114,14 +151,14 @@ const AddAJob = () => {
             </label>
             <div className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'>
               <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
+                selected={startDateDeadline}
+                onChange={(date) => setStartDateDeadline(date)}
               />
             </div>
           </div>
           <div>
             <label className='text-gray-700 dark:text-gray-200'>
-              Job Applicants Number
+              jobApplicantsNumber
             </label>
             <input
               defaultValue={0}
